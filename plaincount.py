ecdc2fcaa9bfd6b52173c3d6e004fc93f36ea904
Colin <cgermain@gmail.com>
2016-12-02 17:42:37 -0500

    Updated gene/protein name and added smarter output filenames.

--- plaincount.py
+++ plaincount.py
@@ -1,29 +1,36 @@
 import csv, sys
-from collections import Counter
+from datetime import datetime
 
-OUT_NAME = "protein_mgf_count.csv"
+TIME_FORMAT =  "%m-%d-%Y_%H-%M-%S"
 
 def main(plain_parse_file):
 	protein_mgf_list = []
 	protein_dict = {}
 	header = []
 
-	with open(plain_parse_file, 'rb') as in_file:
-		csvreader = csv.reader(in_file, delimiter='\t')
-		next(csvreader)
-		for row in csvreader:
-			protein_name = row[16]
-			mgf_filename = row[0]
-			if protein_name not in protein_dict:
-				protein_dict[protein_name] = {}
-			if mgf_filename not in protein_dict[protein_name]:
-				protein_dict[protein_name][mgf_filename] = 1
-				if mgf_filename not in header:
-					header.append(mgf_filename)
-			else:
-				protein_dict[protein_name][mgf_filename]+=1
+	gpm_filename = plain_parse_file.split("-")[0]
 
-	with open(OUT_NAME, 'wb') as out_file:
+	with open(plain_parse_file, "rb") as in_file:
+		csvreader = csv.reader(in_file, delimiter='\t')
+		next(csvreader) #skip header
+		for row in csvreader:
+			# column 0 = filename
+			# column 16 = protein
+			# column 18 = gene
+			protein = row[16] if row[18] == "None" else row[18]
+			mgf_file = row[0]
+			if protein not in protein_dict:
+				protein_dict[protein] = {}
+			if mgf_file not in protein_dict[protein]:
+				protein_dict[protein][mgf_file] = 1
+				if mgf_file not in header:
+					header.append(mgf_file)
+			else:
+				protein_dict[protein][mgf_file]+=1
+
+	timestamp = datetime.now().strftime(TIME_FORMAT)
+
+	with open(gpm_filename+"_count_"+timestamp+".csv", 'wb') as out_file:
 		csvwriter = csv.writer(out_file)
 		csvwriter.writerow(["Protein"]+header)
 		for protein in protein_dict:
